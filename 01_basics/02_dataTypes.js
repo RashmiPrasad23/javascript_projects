f7d25384d1d75541730577ac86d99a684ab78170
RashmiPrasad23 <rashmiprasadroy23@gmail.com>
2023-12-12 10:15:48 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"  //treat all js code as newer version
+// alert(3+3)  we are using nodejs ,not browser,so it wont work here
+
+let name="rashmi"
+let age="22"
+let isLoggedIn=true;
+
+//number -> range(2 to power 53)
+//bigint ->(used in stocks,trading)
+//string -> ""
+//boolean -> true/false
+//null -> standalone value (empty value) eg.  temperature case ,no value detected , toh 0 return nhi kr skte hai qki 0 ek temp hai
+//undefined -> when var. is declatrd but not assigned(abhi value assignn nhi hui hai)
+//symbol ->unique
+
+//object
+console.log(typeof "hitesh")
+console.log(typeof null)  //object
+console.log(typeof undefined)  //undefined
